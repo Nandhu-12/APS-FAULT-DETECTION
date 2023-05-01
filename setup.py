e08cc53855d39d32010013e6e178cae243746dae
Nandhu-12 <nandhusaravanan12@gmail.com>
2023-05-01 09:22:37 +0530

    created new folder and files

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import find_packages,setup
 from typing import List
 
-
+# creating variable for requirements.txt and -e
 REQUIREMENT_FILE_NAME="requirements.txt"
 HYPHEN_E_DOT = "-e ."
 
 def get_requirements()->List[str]:
-    
+    # reading all libraries that are needed
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
         requirement_list = requirement_file.readlines()
     requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
     
+    # removing -e only here as we dont need it in setup.py file
     if HYPHEN_E_DOT in requirement_list:
         requirement_list.remove(HYPHEN_E_DOT)
     return requirement_list
